cd95fbc8524df324639073afbfbcfc1f41fd6d9f
joey <j.anthonym489@gmail.com>
2020-08-24 19:20:09 -0500

    - initial commit

---
+++ src/main/java/driver/pitching/lookup/PitchingStatsLookupController.java
@@ -0,0 +1,13 @@
+package driver.pitching.lookup;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PitchingStatsLookupController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "Application is running!";
+    }
+}
