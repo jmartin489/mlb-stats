e2519c9b076604f518e7f834e6c8953cee065ae0
jmartin489 <jmartin489@users.noreply.github.com>
2020-08-29 08:53:21 -0500

    - remove pitch type list from PitchMetrics - add pitchMetaData map to PitchMetrics - mapping of career (since 2018?) pitch type counts

--- src/main/java/driver/pitching/dto/pitching/PitchMetrics.java
+++ src/main/java/driver/pitching/dto/pitching/PitchMetrics.java
@@ -4,11 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PitchMetrics {
-    private List<PitchType> pitchType;
+    private Map<String, PitchMetaData> pitchMetaDataMap;
 }
